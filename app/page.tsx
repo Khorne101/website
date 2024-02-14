53924bf7dc60d8f438499c93a4711348e68cb319
Khorne <radioactiveaj06@gmail.com>
2024-02-14 12:49:45 -0600

    coming soon

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1> Coming soon </h1>
+      <h1 style={{margin: "auto", textAlign: "center"}}> Coming soon </h1>
     </main>
   );
 }
