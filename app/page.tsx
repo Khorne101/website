44010600448682457e55e90ff73df4456af02b0f
Khorne <radioactiveaj06@gmail.com>
2024-02-14 13:18:05 -0600

    work

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 style={{margin: "auto", textAlign: "center"}}> Coming soon </h1>
+      <header>
+        
+      </header>
     </main>
   );
 }
