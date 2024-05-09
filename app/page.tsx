5197385363ffb912bfc27de1f196681bb8f934d5
Khorne <radioactiveaj06@gmail.com>
2024-05-09 16:39:14 +0000

    more work on the website

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="main">
-      Website Coming Soon!
+      <div className="header">
+        Khorne
+      </div>
+      <div className="content">
+        This is going to be the content of my website
+      </div>
     </div>
   );
 }
