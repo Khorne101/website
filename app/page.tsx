ffcc43b2f1e830e7f89546a85358b515982ed2a4
Khorne <radioactiveaj06@gmail.com>
2024-05-10 01:00:22 +0000

    attempt at boxes

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,53 @@ export default function Home() {
         Khorne
       </div>
       <div className="content">
-        
+        <h3>
+          Projects
+        </h3>
+        <div className="content_box">
+          <box>
+            this is a box
+          </box>
+          <box>
+            this is a box
+          </box>
+          <box>
+            this is a box
+          </box>
+          <box>
+            this is a box
+          </box>
+          <box>
+            this is a box
+          </box>
+          <box>
+            this is a box
+          </box>
+          <box>
+            this is a box
+          </box>
+          <box>
+            this is a box
+          </box>
+          <box>
+            this is a box
+          </box>
+          <box>
+            this is a box
+          </box>
+          <box>
+            this is a box
+          </box>
+          <box>
+            this is a box
+          </box>
+          <box>
+            this is a box
+          </box>
+          <box>
+            this is a box
+          </box>
+        </div>
       </div>
     </div>
   );
