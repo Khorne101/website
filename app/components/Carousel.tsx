cb4f14727ddad2ae285c1e147a7889a114c8d7de
Khorne <radioactiveaj06@gmail.com>
2024-12-02 02:45:24 +0000

    fixed the image carousel to not scroll past the available images, only downside is the number of images is hardcoded

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -2,14 +2,27 @@
 
 import Image from "next/image";
 import { ReactNode, useState } from "react";
-
+ 
 export function Carousel({ images }: { images: ReactNode[] }) {
-    const [currentImage, setCurrentImage] =  useState(0);
+    let [currentImage, setCurrentImage] = useState(0);
+    let numberOfImages = 4;
+    function imageFlip(imageIndex: number) {
+        console.log(imageIndex);
+        if (imageIndex < 0) {
+            setCurrentImage(0);
+            currentImage = 0;
+        } else if (imageIndex >= numberOfImages-1) {
+            setCurrentImage(numberOfImages-1);
+            currentImage = numberOfImages-1;
+        } else {
+            setCurrentImage(imageIndex)
+        }
+    }
     return (
         <div className="ImageCarousel flex">
-            <button className="prev" onClick={() => setCurrentImage(currentImage-1)}>&lt;</button>
+            <button className="prev" onClick={() => imageFlip(currentImage-1)}>&lt;</button>
             <div className="img">{images[currentImage]}</div>
-            <button className="next" onClick={() => setCurrentImage(currentImage+1)}>&gt;</button>
+            <button className="next" onClick={() => imageFlip(currentImage+1)}>&gt;</button>
         </div>
     )
 }
